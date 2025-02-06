656056bbacf0dee45566824f636e7531245b3b11
Siddharth <siddharth1850@gmail.com>
2025-02-06 17:51:52 +0530

    added few modifications to the code

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,18 @@ import errorHandler from "./middleware/globalErrorHandler.middleware";
 
 const app: Express = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  console.log(__dirname + '/public/index.html');
+  res.sendFile('/app/src/public/index.html');
+  
 });
+
 app.use('/api',router)
 app.use(errorHandler)
 
